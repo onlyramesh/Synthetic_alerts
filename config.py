f31e825bba1f631add3af0797793fa05afb41052
onlyramesh <rameshron22@gmail.com>
2020-06-19 22:48:12 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,15 @@
+db_creds = dict(
+    # host='docker.for.mac.host.internal'
+    host='127.0.0.1',
+    # host='localhost',
+    username='username',
+    password='password'
+)
+
+slack_token = dict(
+    SLACK_API_TOKEN='Slack API Token'
+    )
+
+api=dict(
+    # path="/Users/noonacademy/Desktop/API_list.txt"
+)
